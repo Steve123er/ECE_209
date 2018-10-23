206baddc194ba53f051acaa2b72b7a9c38ba2a54
Steve123er <steve123er@gmail.com>
2018-10-22 21:09:58 -0700

    Add files via upload

--- main.m
+++ main.m
@@ -13,30 +13,30 @@
 %% Problem #3
 %Start with the initialization of policy
 pip=policy_matrix_2;
-
+heading=0;
 %Do plot of initial Policy This is problems 3 a,b,c
 pe=0;
 So=[5,2,6];
-path=Plot_Trajectory(pip,So,pe);
+path=Plot_Trajectory(pip,So,pe,heading);
 
-V=policy_Evaluation2(pip,pe); %This is problem 3d.  
+V=policy_Evaluation2(pip,pe,heading); %This is problem 3d.  
 V(5,2,6)%Problem 3e
 
 show_path(So,path)
 
-policy_update(pip,pe) %problem 3f
+policy_update(pip,pe,heading) %problem 3f
 
 %policy iteration
 tic
 pe=0;
 policy_iterations=40;
 for i=1:policy_iterations
-    pip=policy_update(pip,pe);
+    pip=policy_update(pip,pe,heading);
 end
 toc
 
 %New trajectory
-path=Plot_Trajectory(pip,So,pe);
+path=Plot_Trajectory(pip,So,pe,heading);
 show_path(So,path)
 
 %%
@@ -44,9 +44,9 @@ show_path(So,path)
 
 %%Problem #4 Value Iteration
 tic
-[Value_VI, policy_VI]=Value_iteration(pe,policy_iterations);
+[Value_VI, policy_VI]=Value_iteration(pe,policy_iterations,heading);
 toc
-path_VI=Plot_Trajectory(policy_VI,So,pe);
+path_VI=Plot_Trajectory(policy_VI,So,pe,heading);
 
 show_path(So, path_VI)%4b
 
@@ -55,28 +55,30 @@ show_path(So, path_VI)%4b
 pe=0.25;
 %%Problem #5 Additional Senarios
 %5a Plotting the path and value without rerunning policy iteration
-path_pe_0_25=Plot_Trajectory(policy_VI,So,pe);
+path_pe_0_25=Plot_Trajectory(policy_VI,So,pe,heading);
 show_path(So,path_pe_0_25)
-ValRand=policy_Evaluation2(policy_VI,pe);
+ValRand=policy_Evaluation2(policy_VI,pe,heading);
 ValueRandom=ValRand(5,2,6);
 
 
 policy_iterations=40;
 for i=1:policy_iterations
-    pip=policy_update(pip,pe);
+    pip=policy_update(pip,pe,heading);
 end
 toc
-path_pe_0_25=Plot_Trajectory(pip,So,pe);
+path_pe_0_25=Plot_Trajectory(pip,So,pe,heading);
 show_path(So,path_pe_0_25)
 
+heading=1;
 %5b with probability error =0 
+
 pe=0;
 policy_iterations=40;
 for i=1:policy_iterations
-    pip=policy_update(pip,pe);
+    pip=policy_update(pip,pe,heading);
 end
 toc
-path_pe_0_25=Plot_Trajectory_5b(pip,So,pe);
+path_pe_0_25=Plot_Trajectory(pip,So,pe,heading);
 show_path(So,path_pe_0_25)
 
 %5b with probability error =0.25
@@ -84,10 +86,10 @@ tic
 pe=0.25;
 policy_iterations=40;
 for i=1:policy_iterations
-    pip=policy_update(pip,pe);
+    pip=policy_update(pip,pe,heading);
 end
 toc
-path_pe_0_25=Plot_Trajectory_5b(pip,So,pe);
+path_pe_0_25=Plot_Trajectory_5b(pip,So,pe,heading);
 show_path(So,path_pe_0_25)
 
 
